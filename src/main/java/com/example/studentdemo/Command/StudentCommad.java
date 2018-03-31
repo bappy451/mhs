945fb9b917ff671a718b083c379af09208c55989
bappy451 <30324909+bappy451@users.noreply.github.com>
2018-03-31 23:01:28 +0600

    Complete CRUDE operation... Updated With project Lombok...

--- src/main/java/com/example/studentdemo/Command/StudentCommad.java
+++ src/main/java/com/example/studentdemo/Command/StudentCommad.java
@@ -2,8 +2,11 @@ package com.example.studentdemo.Command;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
+@Getter
+@Setter
 public class StudentCommad {
     private Long id;
     private String StudentName;
@@ -12,33 +15,5 @@ public class StudentCommad {
 
     public Byte[] getImage() {
         return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStudentName() {
-        return StudentName;
-    }
-
-    public void setStudentName(String studentName) {
-        StudentName = studentName;
-    }
-
-    public String getBatch() {
-        return Batch;
-    }
-
-    public void setBatch(String batch) {
-        Batch = batch;
     }
 }
--- src/main/java/com/example/studentdemo/Models/Student.java
+++ src/main/java/com/example/studentdemo/Models/Student.java
@@ -1,10 +1,14 @@
 package com.example.studentdemo.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class Student {
-
     @Id
     private Long id;
     private String StudentName;
@@ -12,35 +16,4 @@ public class Student {
     @Lob
     private Byte[] image;
 
-    public Byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(Byte[] image) {
-        this.image = image;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStudentName() {
-        return StudentName;
-    }
-
-    public void setStudentName(String studentName) {
-        StudentName = studentName;
-    }
-
-    public String getBatch() {
-        return Batch;
-    }
-
-    public void setBatch(String batch) {
-        Batch = batch;
-    }
 }
